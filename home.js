9f7adc4bf9a668239b134abf2d3f78bb05a7ecbf
c14712369 <c14712369@gmail.com>
2022-12-02 17:43:45 +0800

    first

---
+++ home.js
@@ -0,0 +1,11 @@
+var divs = $('.slogan   '),
+    limit = 300;  /* scrolltop value when opacity should be 0 */
+
+$(window).on('scroll', function() {
+   var st = $(this).scrollTop();
+
+   /* avoid unnecessary call to jQuery function */
+   if (st <= limit) {
+     divs.css({ 'opacity' : (1 - st/limit)});
+   }
+});

050c12086399bcd911f8c6a65904901bccc47d0e
c14712369 <c14712369@gmail.com>
2022-12-06 15:14:37 +0800

    1206

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+//login
+
 let loginForm = document.querySelector('.login-form-container');
 
 document.querySelector('#login-btn').onclick = () =>{
@@ -8,10 +10,20 @@ document.querySelector('#close-login-btn').onclick = () =>{
   loginForm.classList.remove('active');
 }
 
+// search
+
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
     cartItem.classList.remove('active');
+}
+
+// shopCart
+
+let shopCart = document.querySelector('.shopCart');
+
+document.querySelector('#homeCart').onclick = () =>{
+  shopCart.classList.toggle('active');
 }
